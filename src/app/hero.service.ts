a10d6b40d8e2d3735caf8f4e142b7cf266adea1d
dewinurdin <dewinurdin@gmail.com>
2018-07-06 20:42:34 -0500

    refactored data access & registered HeroService as the provider

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +14,12 @@ export class HeroService {
   //   return HEROES;
   // }
 
-  // of(HEROES) returns an Observable<Hero[]> that emits 
+  constructor(private messageService: MessageService) { }
+  // of(HEROES) returns an Observable<Hero[]> that emits
   // a single value, the array of mock heroes.
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of (HEROES);
   }
-
-  constructor() { }
 }
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,10 @@
+import { MessageService } from './message.service';
+
 export class Hero {
     id: number;
     name: string;
+
+    // "service-in-service" scenario: inject the MessageService into the HeroService
+    // which is injected into the HeroesComponent
+    constructor(private messageService: MessageService) { }
 }
