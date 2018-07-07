7266750bc9682c20868f0ef3b33e7d0009e5548b
dewinurdin <dewinurdin@gmail.com>
2018-07-06 19:28:13 -0500

    created selected Hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// NgModel lives in FormsModule
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -10,7 +12,9 @@ import { HeroesComponent } from './heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // add FormsModule into @NgModule metadata
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
